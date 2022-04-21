1418ba039d6d549543717d048e36cd15ed068714
Gabriele1312 <gsassi2@studenti.uninsubria.it>
2022-04-21 18:45:19 +0200

    Fatto: schermata di login design Da fare in sti giorni: - esternare risorse come colori, stile, padding ecc.. (fondamentale) - aggiungere bottoni facebook e google - schermata di register design sempre

--- app/src/main/java/com/example/mytrainer/LoginActivity.kt
+++ app/src/main/java/com/example/mytrainer/LoginActivity.kt
@@ -2,13 +2,11 @@ package com.example.mytrainer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.animation.AnimationUtils
-import android.widget.ImageView
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
