267c78e09080545b4edeb6131eb9719fb6393329
aldoademi <wiwpoj-hakseS-joqro3>
2022-05-23 19:24:14 +0200

    Prove con il database

--- app/src/main/java/com/example/mytrainer/LoginActivity.kt
+++ app/src/main/java/com/example/mytrainer/LoginActivity.kt
@@ -104,6 +104,8 @@ class LoginActivity : AppCompatActivity(){
         })
 
     }
+
+
     //override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         //super.onActivityResult(requestCode, resultCode, data)
 
