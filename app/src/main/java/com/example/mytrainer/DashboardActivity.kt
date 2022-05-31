7e24f41ce6b39960d0a57bf6f7d641b295707efa
Gabriele1312 <gsassi2@studenti.uninsubria.it>
2022-05-31 11:45:51 +0200

    design pagine

--- app/src/main/java/com/example/mytrainer/DashboardActivity.kt
+++ app/src/main/java/com/example/mytrainer/DashboardActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 
 import com.facebook.login.LoginManager
@@ -13,15 +14,24 @@ import com.google.firebase.auth.FirebaseAuth
 class DashboardActivity : AppCompatActivity() {
 
     var mAuth: FirebaseAuth? = null
+    lateinit var im_personalTrainer: ImageView
+    lateinit var im_user: ImageView
+    lateinit var im_timer: ImageView
+    lateinit var im_exit: ImageView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
-        var btnLogout = findViewById<Button>(R.id.btn_logout)
+        im_personalTrainer = findViewById(R.id.im_personalTrainer)
+        im_user = findViewById(R.id.im_user)
+        im_timer = findViewById(R.id.im_timer)
+        im_exit = findViewById(R.id.im_exit)
 
-        btnLogout?.setOnClickListener{
+       // var btnLogout = findViewById<Button>(R.id.btn_logout)
+
+        im_exit?.setOnClickListener{
 
             mAuth?.signOut()
             LoginManager.getInstance().logOut()
@@ -30,21 +40,21 @@ class DashboardActivity : AppCompatActivity() {
         }
 
 
-        var btnPersonal = findViewById<Button>(R.id.btn_pt)
 
-        btnPersonal.setOnClickListener(){
+
+        im_personalTrainer.setOnClickListener(){
             val personal = Intent(this@DashboardActivity, TrainerActivity::class.java)
             startActivity(personal)
         }
 
-        var btnAtleti = findViewById<Button>(R.id.btn_atleta)
-        btnAtleti.setOnClickListener(){
+
+        im_user.setOnClickListener(){
             val atleti = Intent(this@DashboardActivity, AthleteActivity::class.java)
             startActivity(atleti)
         }
 
-        var btnTimer = findViewById<Button>(R.id.btn_timer)
-        btnTimer.setOnClickListener(){
+
+        im_timer.setOnClickListener(){
             val timer = Intent(this@DashboardActivity, Timer::class.java)
             startActivity(timer)
         }
