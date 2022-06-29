9a2e37f7efbdb30792c784712533be9d8400c814
Gabriele1312 <gsassi2@studenti.uninsubria.it>
2022-06-29 11:32:01 +0200

    prova commit

---
+++ app/src/main/java/com/example/mytrainer/LoginActivityPT.kt
@@ -0,0 +1,159 @@
+package com.example.mytrainer
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.*
+import com.facebook.login.LoginResult
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_login.*
+import java.util.*
+import com.example.mytrainer.DashboardActivity as DashboardActivity1
+import kotlin.toString as toString1
+
+class LoginActivityPT : AppCompatActivity(){
+
+    lateinit var callbackManager: CallbackManager
+    lateinit var mAuth: FirebaseAuth
+    private val TAG = "FacebookAuthentication"
+    lateinit var facebookBtn: Button
+    private lateinit var database: DatabaseReference
+
+
+    //quando l'activity si inizializza, controlla se l'utente è già loggato
+    override fun onStart() {
+        super.onStart()
+        val currentUser = mAuth.currentUser
+
+        var accessToken: AccessToken? = AccessToken.getCurrentAccessToken()
+
+        if(accessToken != null && !accessToken.isExpired){
+            updateUIT()
+        }
+
+//        val utenti = hashMapOf(
+//            "UIDatleti" to (mAuth.uid.toString1())
+//        )
+//        val db = Firebase.firestore
+//        db.collection("Utenti").document("Atleti")
+//            .set(utenti)
+//            .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!") }
+//            .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+//
+//        val usrAtleti = db.collection("Utenti")
+//
+//
+//        val query = usrAtleti.orderBy("UIDatleti").equals("UID")
+//            .get()
+//            .addOnSuccessListener { documents ->
+//                for (document in documents) {
+//                    Log.d(TAG, "${document.id} => ${document.data}")
+//                }
+//            }
+//            .addOnFailureListener { exception ->
+//                Log.w(TAG, "Error getting documents: ", exception)
+//            }
+//        if (query.toString() == UID){
+//            Log.d(TAG, "**************** GGGGGGGG")
+//        }else{
+//            Log.d(TAG, "***************** NOOOOOOOOOOOOOOOO ${query}")
+//        }
+
+    }
+
+    private fun updateUIT() {
+        Toast.makeText(this, " Accesso già effettuato ", Toast.LENGTH_SHORT).show()
+
+        //apre Dashboard
+        val dashboard = Intent(this, TrainerActivity::class.java)
+        startActivity(dashboard)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_pt)
+
+        //inizializza database firebase
+        mAuth= Firebase.auth
+        callbackManager = CallbackManager.Factory.create();
+
+        login_button.setPermissions("email", "public_profile")
+        login_button.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(loginResult: LoginResult) {
+                Log.d(TAG, "facebook:onSuccess:$loginResult")
+                handleFacebookAccessToken(loginResult.accessToken)
+            }
+
+            override fun onCancel() {
+                Log.d(TAG, "facebook:onCancel")
+                // ...
+            }
+
+            override fun onError(error: FacebookException) {
+                Log.d(TAG, "facebook:onError", error)
+                // ...
+            }
+        })
+
+    }
+
+
+    //override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    //super.onActivityResult(requestCode, resultCode, data)
+
+    // Pass the activity result back to the Facebook SDK
+    //callbackManager.onActivityResult(requestCode, resultCode, data)
+    // }
+
+    //se l'utente si è loggato correttamente , riceve un token di accesso, lo scambia per le credenziali di firebase
+    //si autentica in firebase con le credenziali.
+    private fun handleFacebookAccessToken(accessToken: AccessToken) {
+        Log.d(TAG, "HandleFacebookToken $accessToken")
+
+        val credential: AuthCredential = FacebookAuthProvider.getCredential(accessToken.token)
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // login con successo, aggiorna UI con info utente
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = mAuth?.currentUser
+
+                    //btn_facebook.setEnabled(true) //visibilità bottone
+                    updateUI(user)
+
+                } else {
+                    // login fallito, mostra messaggio a utente.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+
+                    //btn_facebook.setEnabled(true)
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+        Log.d(TAG, "Dentro")
+        Toast.makeText(this, " Login effettuato ", Toast.LENGTH_SHORT).show()
+
+        //apre Dashboard
+        val dashboard = Intent(this, TrainerActivity::class.java).also {
+            startActivity(it)
+
+        }
+    }
+}
+
